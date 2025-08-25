125d654f144bf918a39c2a597205ba1071916d3a
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-25 13:21:45 -0500

    Worked on the Product Box

--- src/components/MainProductArea/ProductBox.jsx
+++ src/components/MainProductArea/ProductBox.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './ProductAreaCss.css';
+import image from '../../Images/PunkRockUnicorn.png';
 
 function ProductBox(){
+    const PunkRockUnicorn = '.././Images/PunkRockUnicorn.png';
+    
+    
+    
+    
     return (
         <div className = 'productBox '>
-            <h1 className = ''>Box</h1>
+            <div className = 'rounded-lg overflow-hidden aspect-square'>
+                <img src = {image} />
+            </div>
+            <h1 className = 'text-center'>Box</h1>
+            <h2 className = 'bg-blue-500 mx-2'>DropDown Menu</h2>
+            <div className= 'grid grid-rows-2'>
+                <div><h1>Price</h1></div>
+                <div><h1>SC ICON</h1></div>
+            </div>
+
 
         </div>
     )
