aea9ec514e22b8b53dbdfc41dc63a1841493c11e
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-25 21:28:44 -0500

    added the price variable to the Product Box

--- src/components/MainProductArea/ProductBox.jsx
+++ src/components/MainProductArea/ProductBox.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import './ProductAreaCss.css';
 
 
-/*import image from '../../Images/PunkRockUnicorn.png'; */
+import image from '../../Images/PunkRockUnicorn.png'; 
 import { ProductArray } from '../../productArray.js';
 
 function ProductBox(){
     //const PunkRockUnicorn = '.././Images/PunkRockUnicorn.png';
     
-    /*
+    
     const products =[];
-    for(const prod in productArray){
+    for(const prod in ProductArray){
         const product =(
             <div className = 'productBox '>
             <div className = 'rounded-lg overflow-hidden aspect-square'>
-                <h1>Image goes here</h1>
+                <img src = {image} />
             </div>
             <h1 className = 'text-center'>Box</h1>
             <h2 className = 'bg-blue-500 mx-2'>DropDown Menu</h2>
             <div className= 'grid grid-rows-2'>
-                <div><h1>Price</h1></div>
+                <div><h1>{ProductArray[prod].price}</h1></div>
                 <div><h1>SC ICON</h1></div>
             </div>
 
@@ -28,12 +28,12 @@ function ProductBox(){
         );
         products.push(product);
     }
-        */
+        
     
     
     return (
         <div>
-            <h1>Some text</h1>
+            {products}
         </div>
     )
 }
