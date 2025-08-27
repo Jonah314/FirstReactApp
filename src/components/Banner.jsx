9421fade3fca28bde966a08077ac1e10ce92d035
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-26 21:00:29 -0500

    Added background image to the banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Background from '../Images/StickerBackground.png';
+
 
 function Banner(){
+    
+
     return(
-        <div className ='h-96 bg-red-600 my-8'>
-            <h1>Banner Section</h1>
+        <div className ='h-96 bg-red-600 my-8 '>
+            <div className= 'h-96 overflow-hidden flex items-center justify-center '>
+                <img src = {Background} className=' flex w-full'/>
+            </div>
         </div>
     )
 }
