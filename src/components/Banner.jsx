1253c1c08f7b6e32aec1c0e075efe591923032c7
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-27 06:42:10 -0500

    Added content for the banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,6 +9,10 @@ function Banner(){
         <div className ='h-96 bg-red-600 my-8 '>
             <div className= 'h-96 overflow-hidden flex items-center justify-center '>
                 <img src = {Background} className=' flex w-full'/>
+                <div className='h-64 absolute flex w-full '>
+                    <div className ='h-64 flex w-full bg-black opacity-50 absolute z-1'></div>
+                    <h1 className='text-white text-9xl text-center m-auto font-bold z-10'>Stick With Josh</h1>
+                </div>
             </div>
         </div>
     )
