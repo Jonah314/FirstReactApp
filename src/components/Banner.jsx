b314dd47253bff1524e87d1e0a665a85a1978a55
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-08-27 15:11:56 -0500

    Navbar was appearing behind banner, adjusted the z index to have the nav bar sit on top of the page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,8 +10,8 @@ function Banner(){
             <div className= 'h-96 overflow-hidden flex items-center justify-center '>
                 <img src = {Background} className=' flex w-full'/>
                 <div className='h-64 absolute flex w-full '>
-                    <div className ='h-64 flex w-full bg-black opacity-50 absolute z-1'></div>
-                    <h1 className='text-white md:text-9xl text-8xl text-center m-auto font-bold z-10'>Stick With Josh</h1>
+                    <div className ='h-64 flex w-full bg-black opacity-50 absolute z-20'></div>
+                    <h1 className='text-white md:text-9xl text-8xl text-center m-auto font-bold z-30'>Stick With Josh</h1>
                 </div>
             </div>
         </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar(){
     return (
         
         
-            <div className = 'max-w-full max-h-52 bg-slate-500 text-center mx-auto sticky top-0' >
+            <div className = 'max-w-full max-h-52 bg-slate-500 text-center mx-auto sticky top-0 z-50' >
                 <h1>this is my Nav Bar section</h1>
                 <h1>Just testing that my code is still good</h1>
             </div>
