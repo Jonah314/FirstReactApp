3337cbc29ae2badb26b4df36a9afa16bd70ca68d
Jonathan T Cantu <Jonathant.cantu@gmail.com>
2025-09-02 15:16:30 -0500

    Added new products to the Product Array

--- src/productArray.js
+++ src/productArray.js
@@ -4,6 +4,12 @@ import DrummerBabyImg from './Images/drummer baby.png';
 import PizzaAngelImg from './Images/pizza angel.png';
 import StingRayAlienImg from './Images/StingRay Alien.png';
 import PizzaCannibalImg from './Images/Pizza Cannibal.png';
+import BurgerBoxerImg from './Images/BurgerBoxer.png';
+import TacoEatingSunflowerImg from './Images/TacoEatingSunflower.png';
+import GassyBabyImg from './Images/GassyBaby.png';
+import SunBathingIceCreamImg from  './Images/SunBathingIceCream.png';
+import SpaceManCamelRiderImg from './Images/SpaceManCamelRider.png';
+import AstrounautImg from './Images/Astrounaut.png';
 
 export const ProductArray = {
     PunkRockUnicorn: {
@@ -58,5 +64,50 @@ export const ProductArray = {
         Link: PizzaCannibalImg,
         Tags: ['food','pizza','canibal', 'evil']
 
+    },
+    BurgerBoxer:{
+        image:'imageAddress',
+        title:'Burger Boxer',
+        price: 4.00,
+        description: '2x9',
+        Link:BurgerBoxerImg,
+        Tags:['food', 'burger', 'boxer']
+    },
+    TacoEatingSunflower:{
+        title:'Taco Eating Sunflower',
+        price: 3.44,
+        description: '2x4',
+        Link:TacoEatingSunflowerImg,
+        Tags:['food', 'plant',]
+    },
+    GassyBaby:{
+        title: 'Gassy baby',
+        price: 2.33,
+        description: '2x3',
+        Link:GassyBabyImg,
+        Tags:['baby', 'human']
+    },
+    SunBathingIceCream:{
+        title: 'Sun Bathing Icecream',
+        price:2.33,
+        description: '2x2',
+        Link:SunBathingIceCreamImg,
+        Tags: ['food', 'icecream', 'beach','summer']   
+    },
+    SpaceManCamelRider:{
+        title: 'Space Man Camel Rider',
+        price: 3.22,
+        description: '3x3',
+        Link:SpaceManCamelRiderImg,
+        Tags:['astronaut', 'animal', 'space']
+    },
+
+    Astrounaut:{
+        title: 'Astrounaut',
+        price: 3.22,
+        description: '2x3',
+        Link: AstrounautImg,
+        Tags:['astronaut', 'animal', 'space']
     }
+
 };
